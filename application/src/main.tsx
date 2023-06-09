e99f9b006031b05f5be3ad5ff05fd10615b8ce43
Jakub <xjudiny@fi.muni.cz>
2023-06-09 18:28:48 +0200

    feat: add utils and necessary libraries

--- application/src/main.tsx
+++ application/src/main.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import {AdapterDateFns} from "@mui/x-date-pickers/AdapterDateFns";
+import {LocalizationProvider} from "@mui/x-date-pickers/LocalizationProvider";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <App />
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <App />
+    </LocalizationProvider>
 )
