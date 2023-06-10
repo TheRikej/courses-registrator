4db85b5f728782ab4874eb733dbf11ee2fe6b600
Jakub <xjudiny@fi.muni.cz>
2023-06-10 16:02:43 +0200

    feat: add teachers to CourseSemesterForm

--- application/src/components/CourseSemesterForm.tsx
+++ application/src/components/CourseSemesterForm.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import {Controller, useForm} from 'react-hook-form';
-import { TextField, Button, MenuItem } from '@mui/material';
+import {TextField, Button, MenuItem, FormHelperText} from '@mui/material';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
 import formatSemester from "../utils/semester";
 import {DateTimePicker, TimePicker} from "@mui/x-date-pickers";
 import workDays from "../utils/days";
 import {useParams} from "react-router-dom";
+import Select from "react-select";
 
 const schema = z.object({
   semester: z.string().nonempty('Semester is required.'),
@@ -18,10 +19,11 @@ const schema = z.object({
   timeDay: z.number().optional().nullable(),
   timeHourFrom: z.date().optional().nullable(),
   timeHourTo: z.date().optional().nullable(),
+  teachers: z.number().array().min(1, "At least one teacher is required."),
 }).refine((data) => data.registrationFrom < data.registrationTo, {
   message: "End date cannot be earlier than start date.",
   path: ["registrationTo"],
-}).refine((data) => data.timeHourFrom && data.timeHourTo &&
+}).refine((data) => !data.timeHourFrom || !data.timeHourTo ||
     data.timeHourFrom < data.timeHourTo, {
   message: "End cannot be earlier than start.",
   path: ["timeHourTo"],
@@ -36,6 +38,7 @@ interface CourseSemesterForm {
   timeDay: number | null,
   timeHourFrom: Date | null,
   timeHourTo: Date | null,
+  teachers: [number] | null,
 }
 
 const CourseSemesterForm = () => {
@@ -70,9 +73,18 @@ const CourseSemesterForm = () => {
     }
   ];
 
+  //TODO: fetch users and set the logged in user as the default value in guarantor input
+  const users = [
+    { value: 1523, label: 'Jakub Judiny' },
+    { value: 835, label: 'David Kajan' },
+    { value: 3494, label: 'Michal Drobný' },
+    { value: 7671, label: 'Dalibor Švonavec' },
+  ]
+
 
   const onSubmit = () => {
     const values = getValues();
+    console.log(values);
     const hoursFrom = values.timeHourFrom?.getHours();
     const minutesFrom = values.timeHourFrom?.getMinutes();
     const hoursTo = values.timeHourTo?.getHours();
@@ -87,7 +99,7 @@ const CourseSemesterForm = () => {
       onSubmit={handleSubmit(onSubmit)}
     >
       <h1 className="font-poppins text-2xl m-6 font-bold text-blue-950">
-        List course {code} in a new semester
+        List course {code?.toUpperCase()} in a new semester
       </h1>
       <div className="mx-4 my-1 w-64">
         <TextField
@@ -181,7 +193,7 @@ const CourseSemesterForm = () => {
       </div>
 
       <div className="flex flex-col rounded m-4 border-solid border-2 font-bold">
-        <h2 className="font-poppins text-xl m-4">
+        <h2 className="font-poppins text-xl mx-4 my-2">
           Lectures
         </h2>
         <TextField
@@ -267,8 +279,32 @@ const CourseSemesterForm = () => {
         </div>
       </div>
 
+      <label htmlFor="teachers" className="mx-4 text-sm">Teachers *</label>
+      <Controller
+          control={control}
+          name="teachers"
+          defaultValue={null}
+          render={({ field, }) => (
+              <Select
+                  ref={field.ref}
+                  value={users.filter(c => field.value?.includes(c.value))}
+                  onChange={val => field.onChange(val.map(c => c.value))}
+                  className="w-auto mx-4 mb-2"
+                  isClearable={true}
+                  isSearchable={true}
+                  isMulti={true}
+                  id="teachers"
+                  name="teachers"
+                  options={users}
+              />
+          )}
+      />
+      <FormHelperText error sx={{ margin: '0rem 1.5rem' }}>
+        {errors.teachers?.message}
+      </FormHelperText>
+
       <div className="flex content-center justify-center">
-        <Button type="submit" variant="outlined" sx={{ margin: '0.5rem 2rem' }}>
+        <Button type="submit" variant="outlined" sx={{ margin: '1rem 2rem' }}>
           Submit
         </Button>
       </div>
