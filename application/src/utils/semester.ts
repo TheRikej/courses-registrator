0e23b614710bc17efd40f129ec382be58da55bed
Jakub <xjudiny@fi.muni.cz>
2023-06-10 19:50:45 +0200

    fix: change to united semester format

--- application/src/utils/semester.ts
+++ application/src/utils/semester.ts
@@ -1,5 +1,5 @@
 function formatSemester(year: number, season: string) {
-    return season.toLowerCase() + ' ' + String(year);
+    return season.toLowerCase() + String(year);
 }
 
 export default formatSemester;
