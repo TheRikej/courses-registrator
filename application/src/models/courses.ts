618c946ad187bba05466bba6e2726d9c16a23b0c
Michal Drobny <xdrobny@fi.muni.cz>
2023-06-17 14:48:18 +0200

    fix: Remove unused preferences.

--- application/src/models/courses.ts
+++ application/src/models/courses.ts
@@ -1,31 +1,17 @@
 export interface CoursesFilterData {
-    semester: string | null;
-    faculty: string | null;
-    nameCode: string | null;
-    endrolled: boolean;
-    teaching: boolean;
-  }
+	semester: string | null;
+	faculty: string | null;
+	nameCode: string | null;
+	endrolled: boolean;
+	teaching: boolean;
+}
   
-  export interface PeoplePreferences {
-    /**
-     * Set of people uuids where the user clicked 'Interested'.
-     * Such people can be easily accessed from App bar.
-     */
   
-    interested: Set<string>;
-    /**
-     * Set of people uuids where the user clicked 'Not Interested'.
-     * Such people cannot be shown anymore.
-     */
-  
-    notInterested: Set<string>;
-  }
-  
-  export const defaultCoursesFilterData: CoursesFilterData = {
-    semester: null,
-    faculty: null,
-    nameCode: null,
-    endrolled: false,
-    teaching: false,
-  };
+export const defaultCoursesFilterData: CoursesFilterData = {
+	semester: null,
+	faculty: null,
+	nameCode: null,
+	endrolled: false,
+	teaching: false,
+};
   
