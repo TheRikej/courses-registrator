d5ad486c50dedd1aaba0a9a080ad9653b7907244
Jakub <xjudiny@fi.muni.cz>
2023-06-20 01:12:12 +0200

    feat: add name to loggedUser atom

--- application/src/atoms/loggedUser.tsx
+++ application/src/atoms/loggedUser.tsx
@@ -2,12 +2,13 @@ import { atom } from "recoil";
 
 export interface LoggedUser {
     id: number,
+    name: string,
     admin: boolean,
     teacher: boolean,
     student: boolean,
 }
 
 export const loggedUserAtom = atom<LoggedUser | null>({
-    key: 'TodoList',
-    default: {id: 1, admin: false, teacher: false, student: true},
+    key: 'LoggedUser',
+    default: {id: 1, admin: true, teacher: false, student: true, name: "Jakub Judiny"},
 });
