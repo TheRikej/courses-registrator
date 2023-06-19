747c869b394548a5b6cf15e3c9aada9833c5cda8
David Kajan <xkajan@fi.muni.cz>
2023-06-19 23:31:51 +0200

    feat: Added specific seminar read query

--- back-end/src/controllers/seminar/index.ts
+++ back-end/src/controllers/seminar/index.ts
@@ -1,11 +1,13 @@
 import createSeminar from "./createSeminarAPI"
 import updateSeminar from "./updateSeminarAPI"
 import deleteSeminar from "./deleteSeminarAPI"
-import readAllSeminar from "./readSeminarAllAPI" 
+import readAllSeminar from "./readSeminarAllAPI"
+import readSpecificSeminar from "./readSeminarSpecificAPI"
 
 export default {
     createSeminar,
     updateSeminar,
     deleteSeminar,
     readAllSeminar,
+    readSpecificSeminar,
 }
